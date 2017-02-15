9cb6c0c953236e7b8427ed981cb64710945e3ebc
schwamster <bastian.toepfer@bodoconsult.de>
2017-02-15 23:49:06 +0100

    logs now land in s3

--- lambdas/MoveLogsToS3/src/MoveLogsToS3/Function.cs
+++ lambdas/MoveLogsToS3/src/MoveLogsToS3/Function.cs
@@ -10,6 +10,9 @@ using Amazon.Lambda.Serialization;
 using Amazon.Lambda.Serialization.Json;
 using System.IO;
 using System.IO.Compression;
+using Amazon.S3;
+using Amazon;
+using Amazon.S3.Model;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -33,7 +36,6 @@ namespace MoveLogsToS3
         /// <returns></returns>
         public string FunctionHandler(KinesisEvent kinesisEvent, ILambdaContext context)
         {
-            Console.WriteLine("called");
             HandleKinesisRecord(kinesisEvent);
             return "OK";
         }
@@ -41,15 +43,39 @@ namespace MoveLogsToS3
         public void HandleKinesisRecord(KinesisEvent kinesisEvent)
         {
             Console.WriteLine($"Beginning to process {kinesisEvent.Records.Count} records...");
-
+            var logs = "";
             foreach (var record in kinesisEvent.Records)
             {
                 Console.WriteLine($"Event ID: {record.EventId}");
                 Console.WriteLine($"Event Name: {record.EventName}");
                 string recordData = GetRecordContents(record.Kinesis);
+                logs = logs + recordData;
                 Console.WriteLine($"Record Data:");
                 Console.WriteLine(recordData);
             }
+            //todo: region needs to be read from env vars as well
+            try
+            {
+                using (var client = new AmazonS3Client())
+                {
+                    var key = DateTime.UtcNow.ToString("yyyyMMddHHmmssfff");
+                    Console.WriteLine("key {0}", key);
+                    PutObjectRequest request = new PutObjectRequest()
+                    {
+                        ContentBody = logs,
+                        BucketName = this.destinationBucket,
+                        Key = $"{key}"
+                    };
+                    Console.WriteLine("Created object for bucket");
+                    PutObjectResponse response = client.PutObjectAsync(request).Result;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error while writting to bucket: {0}", ex);
+            }
+
 
             Console.WriteLine("Stream processing complete.");
         }
--- variables.tf
+++ variables.tf
@@ -10,5 +10,5 @@ variable "log_group_name" {
 
 #!see naming convetions of buckets - only lower-case characters
 variable "bucket_name" {
-  default = "logbucket"
+  default = "8cf434d3-7425-41c2-9201-fc8e8d784a86"
 }
